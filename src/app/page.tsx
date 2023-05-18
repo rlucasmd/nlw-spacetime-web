24d0f757511bb6b4d7dc2286028672e109c7bfff
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-17 22:02:57 -0300

    add: initial page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,52 @@
+import { User } from "lucide-react";
+import nlwLogo from "../assets/nlw-spacetime-logo.svg";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex h-[100px] flex-1 bg-red-500">
-      <h1 className="m-auto">Hello word</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover  px-28 py-16 ">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        <div>
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div>
+            <h1>Sua cápsula do tempo</h1>
+            <p>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a
+              href=""
+              className="dealy-150 underline transition hover:text-gray-50"
+            >
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
